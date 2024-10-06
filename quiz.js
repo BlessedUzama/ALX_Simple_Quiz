5149439c32ef7a7906a95a2b78403251147a4216
BlessedUzama <uzamablessed@gmail.com>
2024-10-06 11:23:03 +0100

    first attempt task0

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function checkAnswer() {
+    let correctAnswer = '4'
+   const userAnswer = document.querySelector('input[name="quiz"]:checked').value
+   const feedback = document.getElementById('feedback')
+   if (userAnswer == correctAnswer) {
+    
+    feedback.textContent = "Correct! Well done."
+   } else {
+    feedback.textContent = "That's incorrect. Try again!"
+   }
+}
+
+const submitBtn = document.getElementById("submit-answer")
+submitBtn.addEventListener('click', checkAnswer);
